0ec73d4f3ff68cd80aa6b93a099740051033f7b8
Ilham Satrian <ilhamsatrian@gmail.com>
2017-04-08 12:02:33 +0700

    Update Jurnal8.java

--- Jurnal8.java
+++ Jurnal8.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class Jurnal8 {
     
     public static void main(String[] args) {
-        long hasil = 0;
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukkan Jumlah Deret Fibonacci : ");
+       Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci : ");
         int n = show.nextInt();
         long fib[] = new long[n];
-        
+        long hasil = 0;
+         
         fib[0] = 1;
         fib[1] = 1;
-        
-        for(int i=2; i<n; i++){
+         
+        for(int i = 2; i < n; i++) {
             fib[i] = fib[i-1] + fib[i-2];
         }
         
-        for(int i=0; i<n; i++){
-            hasil = fib[i] + hasil;
+         
+        for (int i = n-1; i >= 0; i--) {
+            System.out.print(fib[i] +  " ");
         }
-        System.out.println(hasil);
+        
     }
+    
 }
